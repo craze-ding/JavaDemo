8426103565502571c811755c95cad2ba5969e3cb
craze-ding <dzjcrae12121@gmail.com>
2021-03-21 20:03:50 +0800

    补充同步锁的应用

--- SynchronizedDemo.java
+++ SynchronizedDemo.java
@@ -1,75 +1,71 @@
 class Ticket implements Runnable //extends Thread
 {
-	private int num=200;
+	private static int num=200;
 	boolean flag=true;
+	//Object obj=new Object();测试锁是否唯一
 	public void sale(){
+		//System.out.println("this:"+this.getClass());打印class文件对象哈希值
 		while(true){
-			synchronized( this/* Ticket.class */){//同步代码块
+			synchronized(/* obj *//* this */Ticket.class){//同步代码块
 				if(num >0){
 				try{
 					Thread.sleep(10);	
 				}
 				catch(InterruptedException in ){					
-				}
-				
+				}				
 				System.out.println(Thread.currentThread().getName()+"....."+"代码块"+"...."+num--);
 			    }
-			}
-			
+			}			
 		}
 	}
-	public void run(){//验证同步函数锁的对象?是this.
-		System.out.println("this:"+this);
+	public void run(){//验证同步函数锁的对象?是this.		
 		if(flag){
 			sale();
 		}
 		else{
-			show1();
+			//show1();
+			show2();
 		}
 	}
-	public synchronized void show1(){//同步函数
-		
+	public synchronized void show1(){//同步函数		
 		while(true){
 			if(num >0){
 				try{
 					Thread.sleep(10);	
 				}
 				catch(InterruptedException in ){					
-				}
-				
+				}				
 				System.out.println(Thread.currentThread().getName()+"....."+"funtion"+"...."+num--);
 				}
 		}
 	}
-	public synchronized void show2(){//静态同步函数
+	public static synchronized void show2(){//静态同步函数的锁是所属字节码文件对象
 		while(true){
 			if(num >0){
 				try{
 					Thread.sleep(10);	
 				}
 				catch(InterruptedException in ){					
-				}
-				
+				}				
 				System.out.println(Thread.currentThread().getName()+"....."+num--);
 				}
-		}
-		
+		}		
 	}
-
 	
 }
 class SynchronizedDmo{
 	public static void main(String[] args) {		
 		Ticket T=new Ticket();//创建线程任务对象
-		System.out.println("T:"+T);
+		//System.out.println("T:"+T);
+		/* Class clazz=T.getClass();
+		Class clacc=Teacher.class; *///获取类的class字节码文件
 		Thread t1=new Thread(T);
 		Thread t2=new Thread(T);		
 		/* Ticket t2=new Ticket();
 		Ticket t3=new Ticket(); */
 		t1.start();		
-		try{Thread.sleep(5);}catch(InterruptedException in){}		
+		try{Thread.sleep(5);}catch(InterruptedException in){}	
 		T.flag=false;
-		t2.start();
-		
+		t2.start();		
 	}
 }
