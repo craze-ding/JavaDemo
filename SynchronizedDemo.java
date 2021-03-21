195c3cd3cc4a99d4553dea094591788f62e42556
craze-ding <dzjcrae12121@gmail.com>
2021-03-21 19:01:03 +0800

    增加同步函数和同步代码块Demo

---
+++ SynchronizedDemo.java
@@ -0,0 +1,75 @@
+class Ticket implements Runnable //extends Thread
+{
+	private int num=200;
+	boolean flag=true;
+	public void sale(){
+		while(true){
+			synchronized( this/* Ticket.class */){//同步代码块
+				if(num >0){
+				try{
+					Thread.sleep(10);	
+				}
+				catch(InterruptedException in ){					
+				}
+				
+				System.out.println(Thread.currentThread().getName()+"....."+"代码块"+"...."+num--);
+			    }
+			}
+			
+		}
+	}
+	public void run(){//验证同步函数锁的对象?是this.
+		System.out.println("this:"+this);
+		if(flag){
+			sale();
+		}
+		else{
+			show1();
+		}
+	}
+	public synchronized void show1(){//同步函数
+		
+		while(true){
+			if(num >0){
+				try{
+					Thread.sleep(10);	
+				}
+				catch(InterruptedException in ){					
+				}
+				
+				System.out.println(Thread.currentThread().getName()+"....."+"funtion"+"...."+num--);
+				}
+		}
+	}
+	public synchronized void show2(){//静态同步函数
+		while(true){
+			if(num >0){
+				try{
+					Thread.sleep(10);	
+				}
+				catch(InterruptedException in ){					
+				}
+				
+				System.out.println(Thread.currentThread().getName()+"....."+num--);
+				}
+		}
+		
+	}
+
+	
+}
+class SynchronizedDmo{
+	public static void main(String[] args) {		
+		Ticket T=new Ticket();//创建线程任务对象
+		System.out.println("T:"+T);
+		Thread t1=new Thread(T);
+		Thread t2=new Thread(T);		
+		/* Ticket t2=new Ticket();
+		Ticket t3=new Ticket(); */
+		t1.start();		
+		try{Thread.sleep(5);}catch(InterruptedException in){}		
+		T.flag=false;
+		t2.start();
+		
+	}
+}
