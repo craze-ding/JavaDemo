628550b6c86214b7f601f9dc567b4eda773d9839
craze-ding <dzjcrae12121@gmail.com>
2021-03-21 20:06:22 +0800

    增加编译时的注意事项

--- PackageDemo.java
+++ PackageDemo.java
@@ -1,7 +1,8 @@
 
 package mypackage;
 import packB.DemoB;//导入packB包中的DemoB类
-import packB.*;//导入packB包中的所有类
+//import packB.*;//导入packB包中的所有类
+//在javac时-cp 指定classpath
 public class PackageDemo{
 	public static void main(String[] args) {
 		/*packA.DemoA d=new packA.DemoA();
